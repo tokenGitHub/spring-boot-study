4b0ba2fe7b8c2667ff069903d2e3cbe9643b767d
hanhe <hanhe12333@qq.com>
2019-01-23 17:25:02 +0800

    对 collect 的实验

--- src/main/java/com/hanhe/study/controller/StreamReductionOperationController.java
+++ src/main/java/com/hanhe/study/controller/StreamReductionOperationController.java
@@ -1,45 +1,49 @@
 package com.hanhe.study.controller;
 
+import com.hanhe.study.domain.ExcelTestDomain;
 import com.hanhe.study.utils.FilterInterface;
 import lombok.extern.log4j.Log4j;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.Date;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Log4j
 @Controller
 @RequestMapping("reduction")
 public class StreamReductionOperationController {
-    private List<String> list = new ArrayList<>();
-    private Map<Integer, String> map = new HashMap<>();
+    private List<ExcelTestDomain> list = new ArrayList<>();
+    private Map<String, Integer> map = new HashMap<>();
 
     private void listInit(){
-        list.clear();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
+        for(int i = 0 ;i < 10 ;i++){
+            ExcelTestDomain testDomain = new ExcelTestDomain();
+            testDomain.setId(i);
+            testDomain.setAge(i +10);
+            testDomain.setName("name~~~" + i);
+            testDomain.setCreateDate(new Date(System.currentTimeMillis()));
+            testDomain.setStatus(i % 3);
+            list.add(testDomain);
+        }
     }
 
     private void mapInit(){
         listInit();
-        list.forEach(s -> map.put(s.length(), s));
+        list.forEach(s -> map.put(s.toString(), s.getId()));
     }
 
     @ResponseBody
     @GetMapping("reduce")
     public Object reduce() throws Exception{
         mapInit();
-        Optional<String> optionalS = list.stream().reduce((s1, s2) -> s1.length() > s2.length() ? s1 : s2);
+        Optional<ExcelTestDomain> optionalS = list.stream().reduce((s1, s2) -> s1.getId() > s2.getId() ? s1 : s2);
         log.info(optionalS.get());
-        log.info(FilterInterface.toString(list.stream().max(Comparator.comparing(String::length))));
-        log.info(list.stream().reduce(0, (sum, str) -> str.length() + sum, (a,b ) -> a + b));
+        log.info(FilterInterface.toString(list.stream().max(Comparator.comparing(ExcelTestDomain::getAge))));
+        log.info(list.stream().reduce(0, (sum, str) -> str.getAge() + sum, (a,b ) -> a + b));
         return map;
     }
 
@@ -47,8 +51,30 @@ public class StreamReductionOperationController {
     @GetMapping("collect")
     public Object collect(){
         mapInit();
+
+        map = list.stream().collect(Collectors.toMap(ExcelTestDomain::getName, ExcelTestDomain::getAge));
+
+        Map map1 = list.stream().collect(
+                Collectors.groupingBy(ExcelTestDomain::getStatus)
+        );
+        map1.forEach((k, v) -> log.info("map1    k : " + k + "  v : " + v));
+
+        Map map2 = list.stream().collect(
+                Collectors.groupingBy(ExcelTestDomain::getStatus, Collectors.counting())
+        );
+
+        map2.forEach((k, v) -> log.info("map2    k : " + k + "  v : " + v));
+
+        Map map3 = list.stream().collect(
+                Collectors.groupingBy(
+                    ExcelTestDomain::getStatus, Collectors.mapping(
+                            ExcelTestDomain::getStatus,Collectors.counting()
+                    )
+                )
+        );
+
+        map3.forEach((k, v) -> log.info("map3    k : " + k + "  v : " + v));
+
         return map;
     }
-
-
 }
